9ac508307cab3250f2d721187637c4fc0986e0a1
lvdelrio <lvdelrio@miuandes.cl>
2021-05-16 03:25:13 -0400

    cuarto intento

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ async function app(state, update, view){
     //FORM (ask)
     const value = await inputForm(model)
     const updateModel = update(value['billAmount'],value['tipPercent'], model)
+    
     state = {
         ...state,
         model: updateModel,
